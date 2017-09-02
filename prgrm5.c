5d4990862cb4a7a64178787d9da2d03bc2408067
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2017-09-02 11:57:44 +0530

    Create prgrm5.c

---
+++ prgrm5.c
@@ -0,0 +1,25 @@
+import java.io*;
+import java.util.*;
+{
+public static void main(strings args[])
+{
+scanner in = new scanner(system.in);
+int a,b,c;
+system.out print ln("enter a number");
+a=in.next int();
+b=in.next int();
+c=in.next int();
+if(a>b&&a>c)
+{
+system.out.print ln("Ais large");
+}
+else if(b>c&&b>a)
+{
+system.out.print ln("B is large");
+}
+else
+{
+system.out.print ln("C is large");
+}
+}
+}
