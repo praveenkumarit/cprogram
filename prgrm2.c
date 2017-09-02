8f74f438bfa3391af14a129dd8ccd08e29bd866e
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2017-09-02 11:44:46 +0530

    Create prgrm2.c

---
+++ prgrm2.c
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter any integer");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("given no is even");
+}
+else
+{
+printf("given no is odd");
+}
+return 0;
+}
