f2270ce0fdab281b72e181fb725b7cf22a7bf685
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2018-01-10 16:24:22 +0530

    Create alphabet.C

---
+++ alphabet.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter any alphabet");
+scanf("%c",&ch);
+if((ch>=65&&ch<=90)!!(ch>=97&&ch<=12))
+{
+printf("enter character is alphabet");
+}
+else
+{
+printf("enter character is not an alphabet");
+}
+getch()
+}
