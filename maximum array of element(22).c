fb1b3cdb4c755cff70655ed9d9cfbcb1b9f4a27c
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2018-01-21 11:47:17 +0530

    Create maximum array of element(22).c

---
+++ maximum array of element(22).c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+#define MAX SIZE 100
+int main()
+{
+    int arr[MAX_SIZE];
+    int i, max,size;
+    printf("Enter size of the array: ");
+    scanf("%d", &size);
+    printf("Enter elements in the array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    max = arr[0];
+    for(i=1; i<size; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    printf("Maximum element = %d\n", max);
+    return 0;
+}
