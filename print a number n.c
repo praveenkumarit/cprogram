d19c8ff9dbacdccbb0b0b72a19d4715aceebe19b
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2018-01-21 10:59:32 +0530

    Create print a number n.c

---
+++ print a number n.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+longint sum=0,i,n;
+printf("give the value of n:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("5*%d=%d",i,5*i);
+}
+getch();
+}
