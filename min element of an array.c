ea1a46be81098602ccd62a47008b2b2e5bdafca9
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2018-01-21 11:53:29 +0530

    Create min element of an array.c

---
+++ min element of an array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100  
+
+int main()
+{
+    int arr[max SIZE];
+    int i,min, size;
+    printf("Enter size of the array: ");
+    scanf("%d", &size);
+    printf("Enter elements in the array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    min = arr[0];
+    for(i=1; i<size; i++)
+    {
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    printf("Minimum element = %d", min);
+
+    return 0;
+}
