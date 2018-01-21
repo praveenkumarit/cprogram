c1260c9e7275902c244b45d3f8a3a8201998b0ce
nandhini2323 <31503963+nandhini2323@users.noreply.github.com>
2018-01-21 11:09:46 +0530

    Create armstrong between 2 intervals.c

---
+++ armstrong between 2 intervals.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int n1,n2,i,temp,num,rem;
+printf("enter two no's(intervals)");
+scanf("%d%d",&n1,&n2);
+printf("armstrong no's between %d and %d are:",n1,n2);
+for(i=n1+1;i<n2;i++)
+{
+temp=i;
+num=0;
+while(temp!=0)
+{
+rem=(temp%10);
+num1=rem*rem*rem;
+temp/=10;
+}
+if(i==num)
+{
+printf("%d",i);
+}
+}
+return 0;
+}
